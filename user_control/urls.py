47dab5f51167ac6e01da22589f368a52676186ce
jana-sanjay <djanarthanan614@gmail.com>
2023-04-06 11:29:35 +0530

    bug fixed

--- user_control/urls.py
+++ user_control/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import LoginView, RegisterView, UserProfileView, MeView
+from .views import LoginView, RegisterView, UserProfileView, MeView, ProfileView
 from rest_framework.routers import DefaultRouter
 
 from rest_framework_simplejwt.views import (
@@ -16,6 +16,7 @@ urlpatterns = [
     path('login', LoginView.as_view()),
     path('register', RegisterView.as_view()),
     path('me', MeView.as_view()),
+    path('myprofile', ProfileView.as_view()),
     path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
